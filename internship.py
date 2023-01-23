0a7e4151ab4abe8e431084d4b8daa30c47e8d5e7
bansal089 <priyanshumvn@gmail.com>
2023-01-23 10:14:42 +0530

    day1

---
+++ internship.py
@@ -0,0 +1,12 @@
+class calculator:
+    def __init__(self, num ):
+        self.number = num
+    def square(self):
+        print(f"the value of {self.num} square is{self.num **2} ")
+    def squareroot(self):
+        pass
+    def cube(self):
+        pass 
+
+a= calculator(3)
+a.square()
