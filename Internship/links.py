3573b347b10149572f79f34aab21947b7082d73d
bansal089 <123165125+bansal089@users.noreply.github.com>
2023-02-23 11:52:49 +0530

    Add files via upload

---
+++ Internship/links.py
@@ -0,0 +1,4 @@
+links = ['https://internshala.com/internship/detail/data-analytics-internship-in-bangalore-at-retranz-infolabs1675392893',
+         'https://internshala.com/internship/detail/python-flask-development-work-from-home-job-internship-at-druidot-consulting-private-limited1675323704',
+         'https://internshala.com/internship/detail/python-development-internship-in-mohali-at-omninos-technologies-international-private-limited1675491687']
+
